2b807c8833b8e4605e634f4fcd68e57cf3d3cb91
Champars <100133970+Champars@users.noreply.github.com>
2024-02-15 19:50:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ var ball = {
 rightX = "";
 rightY = "";
 score = "";
+geme_status = "";
 
 function setup() {
     var canvas = createCanvas(700, 600);
@@ -39,53 +40,60 @@ function setup() {
     pn.on('pose', gotposes);
 }
 
+function startgame() {
+    geme_status = "start";
+    document.getElementById("s").innerHTML = "Game Loaded";
+}
 
 function draw() {
-    image(video, 0, 0, 700, 600);
+    if (geme_status == "start") {
+        image(video, 0, 0, 700, 600);
 
-    if (score > 0.2) {
-        fill("cyan");
-        stroke("cyan");
-        circe(rightX, rightY, 3);
+        if (score > 0.2) {
+            fill("cyan");
+            stroke("cyan");
+            circe(rightX, rightY, 3);
+        }
+        background(0);
+
+        fill("black");
+        stroke("black");
+        rect(680, 0, 20, 700);
+
+        fill("black");
+        stroke("black");
+        rect(0, 0, 20, 700);
+
+        //funtion paddleInCanvas call 
+        paddleInCanvas();
+
+        //left paddle
+        fill(250, 0, 0);
+        stroke(0, 0, 250);
+        strokeWeight(0.5);
+        paddle1Y = mouseY;
+        rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
+
+
+        //pc computer paddle
+        fill("#FFA500");
+        stroke("#FFA500");
+        var paddle2y = ball.y - paddle2Height / 2;
+        rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
+
+        //function midline call
+        midline();
+
+        //funtion drawScore call 
+        drawScore();
+
+        //function models call  
+        models();
+
+        //function move call which in very important
+        move();
     }
-    background(0);
 
-    fill("black");
-    stroke("black");
-    rect(680, 0, 20, 700);
-
-    fill("black");
-    stroke("black");
-    rect(0, 0, 20, 700);
-
-    //funtion paddleInCanvas call 
-    paddleInCanvas();
-
-    //left paddle
-    fill(250, 0, 0);
-    stroke(0, 0, 250);
-    strokeWeight(0.5);
-    paddle1Y = mouseY;
-    rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
-
-
-    //pc computer paddle
-    fill("#FFA500");
-    stroke("#FFA500");
-    var paddle2y = ball.y - paddle2Height / 2;
-    rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
-
-    //function midline call
-    midline();
-
-    //funtion drawScore call 
-    drawScore();
-
-    //function models call  
-    models();
-
-    //function move call which in very important
-    move();
 }
 
 function modelloaded() {
